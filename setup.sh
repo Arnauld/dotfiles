0e173554847f56f2812775403f7c5d5b217b49bb
Arnauld <arnauld.loyer@gmail.com>
2015-09-28 16:38:40 +0200

    add vimerl + fix vim install

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ rm ~/.zshrc
 ln -s ~/.dotfiles/zsh/zshrc.zsh ~/.zshrc
 ln -s ~/.dotfiles/zsh/oh-my-zsh/themes/doubleend.zsh-theme ~/.oh-my-zsh/themes
 
-ln -s ~/.dotfiles/vim/vim ~/.vim
+mkdir -p ~/.vim/tmp
+cp -R  ~/.dotfiles/vim/vim/* ~/.vim
 ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
 
